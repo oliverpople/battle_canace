f08b8fbdf79ac27f149479eb1c5dd3777bea1795
oliverpople <oliverpople@gmail.com>
2017-10-12 14:50:59 +0100

    Adding /names route, view and corresponding test to Battle app.

--- spec/features/infrastructure_spec.rb
+++ spec/features/infrastructure_spec.rb
@@ -1,6 +1,9 @@
-feature 'testing homepage content' do
-  scenario 'user lands on homepage' do
-    visit '/'
-    expect(page).to have_content 'Testing infrastructure working!'
+feature 'takes users name and return new route displaying them' do
+  scenario 'user types in two names to form' do
+    visit  '/'
+    fill_in :player1, with: "Edward"
+    fill_in :player2, with: "Oliver"
+    click_button "Start"
+    expect(page).to have_content "Edward vs Oliver"
   end
 end
